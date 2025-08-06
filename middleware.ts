4792e3e270d9a8e6da6f34271f9dbe766c7e5d72
dillanchen <dillancola@gmail.com>
2025-08-06 11:55:45 +0800

    fixassetsv2

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals, /agent routes and all static files
     '/((?!_next|agent|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Always run for API routes, but exclude /agent
+    '/(api|trpc)(?!/agent)(.*)',
   ],
 };
