d26f24bb01619183a8b12eb608f006b29c4250c3
dillanchen <dillancola@gmail.com>
2025-08-06 12:18:47 +0800

    check

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,13 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // 排除 _next、agent、assets 和所有静态文件
-    '/((?!_next|agent|assets|.*\\.[a-zA-Z0-9]+$).*)',
-    // 处理 API 路由
-    '/api/(.*)',
-    '/trpc/(.*)',
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
+
+
+
+
