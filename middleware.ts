69c9ab044301a1207880024c0ae83a8838c58453
dillanchen <dillancola@gmail.com>
2025-08-06 11:52:29 +0800

    deleteassets

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export default clerkMiddleware();
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|agent|assets|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|agent|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
