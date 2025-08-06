186553bf1be44006c32d27a821b0930c6e9f59cf
dillanchen <dillancola@gmail.com>
2025-08-06 13:24:54 +0800

    addproxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 设置应用的基础路径为 /agent
+  basePath: '/agent',
+  
   // 增加服務器響應時間限制
   serverRuntimeConfig: {
     // Will only be available on the server side
